50853e92850259f871d600910b60a291e541d64f
Gemma <gcarravi@gmail.com>
2026-01-22 15:06:38 +0000

    define variables outside the for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,16 +11,17 @@ const themeToggle = document.querySelector('#themeToggle');
 const swapIcon = document.querySelector('.icon');
 
 // Populates the From and To drop downs
+let selected;
+let option;
 for(let i=0; i<dropList.length; i++){
     for(country in country_code){
         // selecting USD and EUR by default as From and To.
-        let selected;
         if(i ==  0){        //For select drop down
             selected = country == "USD" ? "selected" : "";
         } else if(i  == 1){ // To select drop down
             selected = country == "EUR" ? "selected" : "";
         }
-        let option = `<option value="${country}" ${selected}>${country}</option>`;
+        option = `<option value="${country}" ${selected}>${country}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", option); // inserts options tag inside select tag
     }
 
